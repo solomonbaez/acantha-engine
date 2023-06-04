26523d8589636276af44e877ede351a1aada0871
solomonbaez <solomon.james.baez@gmail.com>
2023-06-04 15:29:57 -0700

    Added Rust/JS color change functionality -> will be interactive

--- engine/examples/game.rs
+++ engine/examples/game.rs
@@ -4,5 +4,8 @@ extern "C" {
 
 fn main() {
     // JS communication requires unsafe declarations
-    unsafe { log_number(4) }
+    unsafe {
+        log_number(4);
+        engine::clear_screen_to_color(0.0, 0.0, 1.0, 1.0);
+    };
 }
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,1 +1,8 @@
+extern "C" {
+    fn js_screen_color(red: f32, green: f32, blue: f32, alpha: f32);
+}
 
+// encase unsafe calls
+pub fn clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32) {
+    unsafe { js_screen_color(red, green, blue, alpha) }
+}
